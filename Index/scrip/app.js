f9a7cc8d02be0830302d5d629b9a871ca4c107bd
Elkin Ferney Rodriguez <erodr144@estudiante.ibero.edu.co>
2026-05-20 16:17:16 +0000

    Implementacion inicial ClinicOs

--- Index/scrip/app.js
+++ Index/scrip/app.js
@@ -1,16 +1,7 @@
-const formulario = document.getElementById("formulario");
-const listaHistorias = document.getElementById("listaHistorias");
+function login(){
+    alert("Inicio de sesión exitoso");
+}
 
-formulario.addEventListener("submit", function(evento) {
-  evento.preventDefault();
-
-  const titulo = document.getElementById("titulo").value;
-  const descripcion = document.getElementById("descripcion").value;
-
-  const item = document.createElement("li");
-  item.innerHTML = `<strong>${titulo}</strong><br>${descripcion}`;
-
-  listaHistorias.appendChild(item);
-
-  formulario.reset();
-});
+function reporte(){
+    alert("Reporte generado correctamente");
+}
